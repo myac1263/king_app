10f10e563164ba5609236cb4be141740bac53e4c
myac1263 <myac1263@gmail.com>
2025-01-16 09:30:39 -0500

    trial

---
+++ Happy_Page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import io
+
+def display_Happy_Page_panel():
+    #header image
+    st.image("https://i.imgur.com/qh7yOt2.jpeg", use_container_width=True)
+    st.markdown("---")
+    st.title("Welcome to your happy page!")
